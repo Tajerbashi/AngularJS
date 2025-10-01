f7daa700c73805fbbb4da601712142271e66a50b
KTajerbashi <kamrantajerbashi@gmail.com>
2025-10-01 14:27:23 +0330

    #Feature Add Base Structure of Server

---
+++ src/app.js
@@ -0,0 +1,21 @@
+angular.module('myApp', ['ngRoute'])
+    .controller('MainController', ['$scope', '$http', function ($scope, $http) {
+        var vm = this;
+        vm.title = 'AngularJS with Node.js Server';
+        vm.message = 'Hello World!';
+        vm.serverData = null;
+
+        // Make $scope available for header
+        $scope.appTitle = 'My AngularJS App';
+
+        vm.getData = function () {
+            alert("Get Data");
+            //   $http.get('/api/data')
+            //     .then(function(response) {
+            //       vm.serverData = response.data;
+            //     })
+            //     .catch(function(error) {
+            //       console.error('Error fetching data:', error);
+            //     });
+        };
+    }]);
