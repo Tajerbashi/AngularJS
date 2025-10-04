d821dee4dd5d0cafd53f4f93735f644503dd2c88
KTajerbashi <kamrantajerbashi@gmail.com>
2025-10-04 14:10:49 +0330

    #Feature Add Routes

--- src/app/configs/app.config.js
+++ src/app/configs/app.config.js
@@ -1,24 +1,39 @@
-angular.module('myApp').config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-  
+angular.module('myApp').config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+
   // Enable HTML5 mode for clean URLs (remove the '#' from URL)
   // Note: This requires server-side configuration for deep links
   // $locationProvider.html5Mode(true);
-  
+
   $routeProvider
     .when('/home', {
       templateUrl: 'app/pages/dashboard/home/home.view.html',
       controller: 'HomeController',
-      controllerAs: 'vm'
+      controllerAs: 'vm',
+      css: 'app/pages/dashboard/home/home.style.css'
     })
     .when('/about', {
       templateUrl: 'app/pages/dashboard/about/about.view.html',
       controller: 'AboutController',
-      controllerAs: 'vm'
+      controllerAs: 'vm',
+      css: 'app/pages/dashboard/about/about.style.css'
     })
     .when('/contact', {
       templateUrl: 'app/pages/dashboard/contact/contact.view.html',
       controller: 'ContactController',
-      controllerAs: 'vm'
+      controllerAs: 'vm',
+      css: 'app/pages/dashboard/contact/contact.style.css'
+    })
+    .when('/forms', {
+      templateUrl: 'app/pages/common/forms/forms.view.html',
+      controller: 'FormsController',
+      controllerAs: 'vm',
+      css: 'app/pages/common/forms/forms.style.css'
+    })
+    .when('/form-validations', {
+      templateUrl: 'app/pages/common/formValidations/formValidations.view.html',
+      controller: 'FormValidationsController',
+      controllerAs: 'vm',
+      css: 'app/pages/common/formValidations/formValidations.style.css'
     })
 
     .otherwise({
